5df39f109060463cb20254e1fa254d4172e3d64b
UM ELEFAANTE INCOMODA MUITA GENTE <159184945+PaulinhaAloka@users.noreply.github.com>
2025-09-23 11:20:04 -0300

    23/09

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado=document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual é o primeiro pensamento",
+    enunciado: "Você prefere sair com um pequeno grupo de amigos ou com um grande grupo?",
     alternativas:[
         {
-        texto:"isso é assustador",
+        texto:"Pequeno grupo.",
         afirmacao:"afirmação"
         },
         {
-       texto:"isso é maravilhoso",
+       texto:"Grande grupo.",
        afirmacao:"afirmação"
         }
 
@@ -22,13 +22,13 @@ const perguntas = [
 
 {
     
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma",
+        enunciado: "Você é o tipo de pessoa que busca ajuda quando necessário?",
         alternativas:[{
-            texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            texto:"Apenas quando realmente necessário.",
             afirmacao:"afirmação"
         },
         {        
-           texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+           texto:"Procuro ajuda em qualquer situação, afinal, tudo se torna mais leve",
            afirmacao:"afirmação"
         }        
         ]
@@ -37,14 +37,14 @@ const perguntas = [
 
     {
     
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Você tem uma vida despreocupada e lida com as situações conforme elas surgem?",
         alternativas:[
             {
-            texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            texto:"Busco ter a solução dos problemas antes mesmo que eles surjam, estando preparado para tudo.",
             afirmacao:"afrimação"
             },
             {
-            texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+            texto:"Sendo resiliente, a maior parte dos problemas são solucionáveis. Então, se em algum momento surgir um equívoco, resolverei na hora o que fazer.",
             afirmacao:"afirmação"
             }
         ]
