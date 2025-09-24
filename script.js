307fec02aace441f325f19e68ea4d73969e783f8
UM ELEFAANTE INCOMODA MUITA GENTE <159184945+PaulinhaAloka@users.noreply.github.com>
2025-09-24 12:46:59 -0300

    24/09

--- script.js
+++ script.js
@@ -1,122 +1,116 @@
-const caixaPrincipal=document.querySelector(".caixa-principal");
-const caixaPerguntas=document.querySelector(".caixa-perguntas");
-const caixaAlternativas=document.querySelector(".caixa-alternativas");
-const caixaResultado=document.querySelector(".caixa-resultado");
-const textoResultado=document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-{
-    enunciado: "Você prefere sair com um pequeno grupo de amigos ou com um grande grupo?",
-    alternativas:[
-        {
-        texto:"Pequeno grupo.",
-        afirmacao:"afirmação"
-        },
-        {
-       texto:"Grande grupo.",
-       afirmacao:"afirmação"
-        }
-
-    ]
-},
-
-{
-    
-        enunciado: "Você é o tipo de pessoa que busca ajuda quando necessário?",
-        alternativas:[{
-            texto:"Apenas quando realmente necessário.",
-            afirmacao:"afirmação"
-        },
-        {        
-           texto:"Procuro ajuda em qualquer situação, afinal, tudo se torna mais leve",
-           afirmacao:"afirmação"
-        }        
-        ]
-    },
-    
-
     {
-    
-        enunciado: "Você tem uma vida despreocupada e lida com as situações conforme elas surgem?",
-        alternativas:[
+        enunciado: "Se você colocou a culpa de algo em uma pessoa e depois descobriu que ela era inocente, você pede desculpas ou permanece com o mesmo posicionamento?",
+        alternativas: [
             {
-            texto:"Busco ter a solução dos problemas antes mesmo que eles surjam, estando preparado para tudo.",
-            afirmacao:"afrimação"
+                texto: "Permaneço sem voltar atrás",
+                afirmacao: ""
             },
             {
-            texto:"Sendo resiliente, a maior parte dos problemas são solucionáveis. Então, se em algum momento surgir um equívoco, resolverei na hora o que fazer.",
-            afirmacao:"afirmação"
+                texto: "Peço desculpas",
+                afirmacao: "Você admite seus erros e tem coragem e responsabilidade de arcar com as consequências disso."
             }
         ]
     },
-    
     {
-    
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas:[
+        enunciado: "Já deixou de falar com alguém por causa de posicionamento político?",
+        alternativas: [
             {
-            texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            afirmacao:"afirmação"
-        },
-        {
-            texto:"Criar uma imagem utilizando um gerador de imagem de IA.",
-            afirmacao:"afirmação"
-        }
+                texto: "Nunca.",
+                afirmacao: "Você reconhece que opiniões existem para se diferenciar e não há problema nenhum nisso."
+            },
+            {
+                texto: "Sim.",
+                afirmacao: ""
+            }
         ]
     },
-
     {
-    
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas:[
+        enunciado: "Você já deixou de fazer algo por medo do que as pessoas poderiam achar?",
+        alternativas: [
             {
-            texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            afirmacao:"afirmação"
+                texto: "Já.",
+                afirmacao: ""
             },
-        {
-            texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-            afirmacao:"afirmação"
-        }
+            {
+                texto: "Não.",
+                afirmacao: "Você não deixa as opiniões alheias te afetarem."
+            }
+        ]
+    },
+    {
+        enunciado: "Em uma briga, entre ter razão ou ficar em paz com alguém, você prefere ter razão?",
+        alternativas: [
+            {
+                texto: "Prefiro ter razão.",
+                afirmacao: ""
+            },
+            {
+                texto: "Prefiro ficar em paz.",
+                afirmacao: "Você sabe que não precisa provar nada a ninguém e não insiste no erro."
+            }
+        ]
+    },
+    {
+        enunciado: "Quando alguém te conta algo pedindo para não contar pra ninguém, você conta pra outra pessoa pedindo para ela não contar pra ninguém também?",
+        alternativas: [
+            {
+                texto: "Normalmente, não.",
+                afirmacao: "Você tem consciência que agora assume um compromisso com a pessoa que te contou e não deve decepcioná-la."
+            },
+            {
+                texto: "Normalmente, sim.",
+                afirmacao: ""
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal = " ";
 
 function mostraPergunta(){
     if (atual >= perguntas.length){
         mostraResultado();
         return;
     }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
-    }
-    
-    function mostraAlternativas(){
+}
+
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa.texto;
-    botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
-    }
-     }
+   }
 
-     function respostaSelecionada(opcaoSelecionada){
-        const afirmacoes = opcaoSelecionada. afirmacoes;
-        historiaFinal += afirmacoes + "" ;
-        atual++;
-        mostraPergunta();
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes +"" ;
+    atual++;
+    mostraPergunta();
 
-     }
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se você for uma pessoa com uma ótima moral e um bom senso de certo e errado, você deve ter respondido que...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 
-     function mostraResultado(){
-        caixaPerguntas.textContent = "Em 2049...";
-        textoResultado.textContent = historiaFinal;
-        caixaAlternativas.textContent = "";
-     }
-    
-     mostraPergunta();
+}
+
+mostraPergunta();
+
